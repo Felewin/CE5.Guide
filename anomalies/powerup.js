65e6b27a7ca4550f6b18ae9c37a14b8cfde14b2a
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-23 13:30:52 -0700

    Update powerup.js

--- anomalies/powerup.js
+++ anomalies/powerup.js
@@ -1,8 +1,8 @@
 registerAnomalySpecificInfo('powerup', {
-    content: `<img src="anomalies/powerup.gif" alt="Powerup animation" class="fullwidth">
-    Check out some of the best real footage of powerups!
+    content: `Check out some of the best real footage of powerups!
     ${createYouTubeEmbed('cbYj_IrX_vA', {
         listId: 'PLCXiD1JyNd6XhegrLOYWwGvXj5B5KG0HD',
         title: 'The CE-5 Protocol - Port Austin, Michigan USA'
-    })}`
+    })}
+    <img src="anomalies/powerup.gif" alt="Powerup animation" class="fullwidth">`
 }); 
