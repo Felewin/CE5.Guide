08ebae56aaf7fecc78fbac31a27d42b0a4ed866b
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-23 17:53:51 -0700

    Update wobbler.js

--- anomalies/wobbler/wobbler.js
+++ anomalies/wobbler/wobbler.js
@@ -1,3 +1,3 @@
 registerAnomalySpecificInfo('wobbler', {
-    content: "Wobblers don't quite move in straight lines like Movers do, instead moving rather sinusoidally, subtly weaving/winding/wobbling side to side as they go. Satellites don't do that!"
+    content: "Wobblers don't quite move in straight lines like Movers do, instead moving rather sinusoidally, subtly weaving/winding/wobbling side to side as they go. Satellites don't wobble!"
 });
