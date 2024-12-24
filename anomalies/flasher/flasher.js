32da965fb8f648f228c270ad7b9d01efe4f03c82
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-23 18:50:12 -0700

    flasher

---
+++ anomalies/flasher/flasher.js
@@ -0,0 +1,3 @@
+registerAnomalySpecificInfo('flasher', {
+    content: "Flashers often follow straight paths like movers do, and only become visible in brief flashes, not always very bright, and oftentimes on a regular interval."
+}); 
