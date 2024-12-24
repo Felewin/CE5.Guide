92884473653e21532283104261d512246f6eaebc
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-23 20:29:40 -0700

    Update flashbulb.js

--- anomalies/flashbulb/flashbulb.js
+++ anomalies/flashbulb/flashbulb.js
@@ -1,5 +1,5 @@
 registerAnomalySpecificInfo('flashbulb', {
-    content: "A flashbulb is a brief, likely round flash of incredibly bright light. Unlike camera flashes or other artificial lights, they have an otherworldly brilliance and often occur without any sound."
+    content: "A flashbulb is a brief, apparently round flash of incredibly bright light. Unlike camera flashes or other artificial lights, they have an otherworldly brilliance and often occur without any sound."
 });
 
 // Register flashbulb as uncontrollable
