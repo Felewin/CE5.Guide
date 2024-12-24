de2acb31791fad67907cdb25e2903fa31ee148c6
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-23 19:07:56 -0700

    gifs update

--- anomalies/powerup/powerup.js
+++ anomalies/powerup/powerup.js
@@ -4,6 +4,9 @@ registerAnomalySpecificInfo('powerup', {
         listId: 'PLCXiD1JyNd6XhegrLOYWwGvXj5B5KG0HD',
         title: 'The CE-5 Protocol - Port Austin, Michigan USA'
     })}
+    <a href="https://youtu.be/M0sY6FdlNeQ?t=188" target="_blank">
+        <img src="anomalies/powerup/Examples of CE-5 Footage.gif" class="fullwidth">
+    </a>
     <img src="anomalies/powerup/best-ufo-footage-they-are-star-people-we-know-who-they-are-unci-keya.gif" class="fullwidth">
     <img src="anomalies/powerup/UFO_Best_Power_UP_July_29_Wow_21.gif" class="fullwidth">`
 }); 
