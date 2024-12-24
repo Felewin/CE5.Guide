b4f31f3c7d508fcd2a2ecb57478819bc29a36f34
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-23 19:08:28 -0700

    removed yt vid

--- anomalies/powerup/powerup.js
+++ anomalies/powerup/powerup.js
@@ -1,9 +1,5 @@
 registerAnomalySpecificInfo('powerup', {
     content: `Check out some of the best real footage of powerups!
-    ${createYouTubeEmbed('cbYj_IrX_vA', {
-        listId: 'PLCXiD1JyNd6XhegrLOYWwGvXj5B5KG0HD',
-        title: 'The CE-5 Protocol - Port Austin, Michigan USA'
-    })}
     <a href="https://youtu.be/M0sY6FdlNeQ?t=188" target="_blank">
         <img src="anomalies/powerup/Examples of CE-5 Footage.gif" class="fullwidth">
     </a>
