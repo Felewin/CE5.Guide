16626451598f3d3d208262088bb16908bb809d9e
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-23 17:53:49 -0700

    Update streaker.js

--- anomalies/streaker/streaker.js
+++ anomalies/streaker/streaker.js
@@ -1,5 +1,5 @@
 registerAnomalySpecificInfo('streaker', {
-    content: "A streaker could be a meteor (a “shooting star”), unless it has a distinguishing quality like changing direction midflight."
+    content: 'A streaker could be a meteor (a shooting star), unless it has a distinguishing quality like changing direction midflight, or telepathic communication, for instance.'
 });
 
 // Register streaker as uncontrollable
