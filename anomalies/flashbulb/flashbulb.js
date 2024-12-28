014ff5cb0ef4466a9ef0032f7f2c5075954b5b61
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-27 17:04:51 -0700

    fixing links sometimes not registering mouse hover

--- anomalies/flashbulb/flashbulb.js
+++ anomalies/flashbulb/flashbulb.js
@@ -1,5 +1,5 @@
 registerAnomalySpecificInfo('flashbulb', {
-    content: `A flashbulb is a brief, apparently round flash of incredibly bright light, usually occuring without any sound. Unlike camera flashes or other artificial lights, they have an otherworldly brilliance that is pure and beautiful. <a href="https://www.youtube.com/watch?v=9SLdn1gET88&list=PLnrEt2fIdZ0baV2rjIp6mILJsUIbdgKf2&t=421s" target="_blank">The light comes from a source and materials that aren't manufacturable; the elements are so pure that even in the vacuum of space, our humans could not manufacture it.</a>`
+    content: `A flashbulb is a brief, apparently round flash of incredibly bright light, usually occuring without any sound. Unlike camera flashes or other artificial lights, they have an otherworldly brilliance that is pure and beautiful. <span class="link-wrapper"><a href="https://www.youtube.com/watch?v=9SLdn1gET88&list=PLnrEt2fIdZ0baV2rjIp6mILJsUIbdgKf2&t=421s" target="_blank">The light comes from a source and materials that aren't manufacturable; the elements are so pure that even in the vacuum of space, our humans could not manufacture it.</a></span>`
 });
 
 // Register flashbulb as uncontrollable
