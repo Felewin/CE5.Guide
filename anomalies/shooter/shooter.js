31f163382c0fb0c8d5d4b940eff90f92ce169cb6
Hunter-Bobeck <hunterbobeck@gmail.com>
2024-12-27 17:20:58 -0700

    shooter

---
+++ anomalies/shooter/shooter.js
@@ -0,0 +1,6 @@
+registerAnomalySpecificInfo('shooter', {
+    content: 'A shooter could be a meteor (a shooting star), unless it has a distinguishing quality like changing direction midflight, or telepathic communication, for instance.'
+});
+
+// Register shooter as uncontrollable
+registerUncontrollableType('shooter'); 
